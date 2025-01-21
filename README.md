e92a4579bca48ded7336e76317bc526de695b623
Viktor <n.viktor.p@gmail.com>
2025-01-21 16:25:30 +0300

    Updates

--- README.md
+++
@@ -1,4 +0,0 @@
-# Todo for Mindbox
-[Публикация](https://viperouss14.github.io/Todo-Mindbox/)
-
-For run use `npm i && npm run start`
--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import { ChakraProvider } from '@chakra-ui/react'
-
-createRoot(document.getElementById('root')!).render(
-  <ChakraProvider>
-    <App />
-  </ChakraProvider>,
-)
